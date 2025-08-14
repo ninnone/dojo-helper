d8ceb0351a770efbcae891d4b18c4e9dc46dbb43
ninnone <donfackninnone8@gmail.com>
2025-08-14 18:23:06 +0200

    Initialisation

---
+++ app/News/LatestNews/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LatestNews = () => {
+  return <div> Code </div>;
+};
+
+export default LatestNews;
---
+++ app/tickets/[id]/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main>
+      <h2 className="text-"></h2>
+      <p>Not foundS</p>
+      <p></p>
+    </main>
+  );
+}
